4ba167225ec9f9380350510961cd80c0c6834f73
shaikot07 <saifulislamshaikat007@gmail.com>
2024-11-08 00:42:41 +0600

    solve all problem but not Satisfy with 08 number problem

--- src/problem-5.ts
+++ src/problem-5.ts
@@ -1,5 +1,12 @@
 {
     // 
+function getProperty<T,S extends keyof T>(obj:T,key:S):T[S]{
+return obj[key]
+}
+
+const person = { name: "Alice", age: 30 };
+// console.log(getProperty(person, "name"));
+// console.log(getProperty(person, "age"));
 
 
 
@@ -7,8 +14,5 @@
 
 
 
-
-
-    
     // 
 }
