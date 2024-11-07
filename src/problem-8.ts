4b10819aaa395a9b4f44f168b01ba4a5cb6bd300
shaikot07 <saifulislamshaikat007@gmail.com>
2024-11-08 00:44:30 +0600

    commit the console.log

--- src/problem-8.ts
+++ src/problem-8.ts
@@ -8,7 +8,7 @@
 
 
       const person = { name: "Alice", age: 25, email: "alice@example.com" };
-      console.log(validateKeys(person, ["name", "age"]));
+    //   console.log(validateKeys(person, ["name", "age"]));
       
 
     //
