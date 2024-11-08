729e5a7f9f5629ef9f195554cc3706c227244429
shaikot07 <saifulislamshaikat007@gmail.com>
2024-11-08 16:14:22 +0600

    updated

--- src/problem-8.ts
+++ src/problem-8.ts
@@ -2,13 +2,14 @@
     //
 
 
-    function validateKeys<T>(obj: T, keys: (keyof T)[]): boolean {
+    function validateKeys<T extends object>(obj: T, keys: (keyof T)[]): boolean {
         return keys.every(key => key in obj);
       }
 
 
       const person = { name: "Alice", age: 25, email: "alice@example.com" };
-    //   console.log(validateKeys(person, ["name", "age"]));
+      console.log(validateKeys(person, ["name", "age"]));
+  
       
 
     //
